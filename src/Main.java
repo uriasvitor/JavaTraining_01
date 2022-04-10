c2caa180a5909faa55aa85bfd835d0453a99a3f8
SAIRU <uriasvitor@gmail.com>
2022-04-10 00:44:44 -0300

    ControleRemoto Java Treino

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+        controle.maisVolume();
+        controle.ligar();
+        controle.ligarMudo();
+        controle.abrirMenu();
+        controle.fecharMenu();
+    }
+}
